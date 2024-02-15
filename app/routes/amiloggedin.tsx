9beec0bc793fbb099b8c80b8ec58c6e8422d4fcc
Blake Taylor <sephnescence@gmail.com>
2024-02-16 09:06:07 +1000

    use html entity apostrophe

--- app/routes/amiloggedin.tsx
+++ app/routes/amiloggedin.tsx
@@ -10,7 +10,7 @@ export default function Blake() {
         <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
           <div className="max-w-[600px] w-full mx-auto">
             <h1 className="text-6xl mb-4">The best journal app, period.</h1>
-            <p className="text-2xl text-white/60 mb-4">You're all set!</p>
+            <p className="text-2xl text-white/60 mb-4">You&apos;re all set!</p>
           </div>
         </div>
       </SignedIn>
