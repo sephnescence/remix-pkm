30e0504f32c921527a023e52b104cd8d9deed4d4
Blake Taylor <sephnescence@gmail.com>
2024-04-25 20:46:40 +1000

    Fix types

--- app/routes/_dashboard+/_dashboard.tsx
+++ app/routes/_dashboard+/_dashboard.tsx
@@ -1,7 +1,4 @@
-import { RedirectToSignIn, SignedIn, SignedOut, UserButton } from '@clerk/remix'
-import { Link, Outlet } from '@remix-run/react'
-import PkmIcon from '~/components/icons/PkmIcon'
-import TrashIcon from '~/components/icons/TrashIcon'
+import { Outlet } from '@remix-run/react'
 import TopNav from '~/components/nav/TopNav'
 
 export default function DashboardBase() {
