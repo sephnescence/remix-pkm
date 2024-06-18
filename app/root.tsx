3666a65e1b99140e39b810efa5eb75000cb04e45
Blake Taylor <sephnescence@gmail.com>
2024-06-18 12:38:48 +1000

    Get the Remix version deployed

--- app/root.tsx
+++ app/root.tsx
@@ -23,7 +23,7 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: styles },
 ]
 export const meta: MetaFunction = () => [
-  { title: 'New Remix App' },
+  { title: 'Rethought' },
   { charset: 'utf-8' },
   { viewport: 'width=device-width,initial-scale=1' },
 ]
